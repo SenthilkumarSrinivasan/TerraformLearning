18c51831f23bd7f05c3a05f0d284a71de7863dbe
Senthilkumar Srinivasan <senthils@bn.com>
2024-07-17 22:50:10 +0530

    adding initial version of files

---
+++ InitialTest/variables.tf
@@ -0,0 +1,31 @@
+variable "filename" {
+    default = "meow.txt"
+    type = string
+    description = "this is filename"
+}
+
+variable "petcontent" {
+    default = "i am the file pet with name as meow !!"  
+}
+
+variable "dogPetFilename" {
+    default = "lollloll.txt"
+}
+
+variable "dogPetContent" {
+    default = "i am the file pet with name as loll !!"  
+}
+
+variable "dogNamePrefix" {
+    # default = "mr"  
+}
+
+variable "dogNameSeparator" {
+    # default = "-"  
+}
+
+variable "dogNameLength" {
+    # default = 2 
+}
+
+
