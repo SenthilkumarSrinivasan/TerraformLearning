199208515235cbec286b1c2a02e361dcf5983a06
SenthilkumarSrinivasan <SenthilkumarSrinivasan@users.noreply.github.com>
2024-07-18 22:00:43 +0530

    Initialtest (#2)
    * adding initial version of files
    
    * Pushing the intial changes
    
    ---------
    
    Co-authored-by: Senthilkumar Srinivasan <senthils@bn.com>

--- InitialTest/main.tf
+++ InitialTest/main.tf
@@ -1,26 +1,30 @@
 
 
 resource "local_file" "pet" {
-    filename = var.filename
-    content = "content is coming from random which is ${random_pet.dogName.id}"
-    depends_on = [ random_pet.dogName ]
+  filename   = var.filename
+  content    = "content is coming from random which is ${random_pet.dogName.id}"
+  depends_on = [random_pet.dogName]
+  file_permission =  "0444"
+  lifecycle {
+    ignore_changes = [ file_permission ]
+  }
 }
 
 resource "local_file" "dogPet" {
-    filename = var.dogPetFilename
-    content = var.dogPetContent  
+  filename = var.dogPetFilename
+  content  = var.dogPetContent
 }
 
 resource "random_pet" "dogName" {
-    prefix = var.dogNamePrefix
-    separator = var.dogNameSeparator
-    length = var.dogNameLength
+  prefix    = var.dogNamePrefix
+  separator = var.dogNameSeparator
+  length    = var.dogNameLength
 }
 
 output "dogName" {
-    value = random_pet.dogName.id
+  value = random_pet.dogName.id
 }
 
 output "sampleContent" {
-    value = var.dogNamePrefix
+  value = var.dogNamePrefix
 }
